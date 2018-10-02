42eee042577d16a6a175cbec5f05b0affc0920ff
Paul Maxson <paul.f.maxson@gmail.com>
2018-10-01 23:46:53 -0400

    Cleaned up error passes fetch error handling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,89 @@ import React, { Component } from 'react';
 import './App.css';
 import moment from 'moment';
 
-const getCurrentPosition = (options = {}) => (
-  new Promise( (resolve, reject) => (
-    navigator.geolocation.getCurrentPosition(resolve, reject, options)
-  ))
+const getCurrentPosition = async (options = {}) => (
+  new Promise( (resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(resolve, reject, options);
+  })
 );
 
-// TODO: Request position only in response to user gesture
+const updatePosition = async () => {
+
+  // Set getCurrentPosition options
+  const options = {
+    enableHighAccuracy: true,
+    timeout: 4000,
+    maximumAge: 0
+  };
+
+  // Access getCurrentPosition API
+  let position;
+
+  while (true) {
+
+    try {
+      position = await getCurrentPosition(options);
+      break; // Leave while if no error
+    } catch (e) {
+
+      if (e.__proto__.constructor.name === "PositionError") {
+
+        if (e.code === 1 // PERMISSION_DENIED
+          && !window.confirm( // 'Cancel' --> false
+            "We need your permission to access your location information! Try again?"
+          )
+        ) break; // Leave while
+
+        else if (e.code === 2 // POSITION_UNAVAILABLE
+          && !window.confirm( // 'Cancel' --> false
+            "The acquisition of your geolocation failed because at least one source of position on your machine machine didn't work! Try again?"
+          )
+        ) break; // Leave while
+
+        else if (e.code === 3 // TIMEOUT
+          && !window.confirm( // 'Cancel' --> false
+            "We're having trouble finding you! Try again?"
+          )
+        ) break; // Leave while
+
+      } else throw e; // Leave while
+    }
+  }
+  return position;
+}
+
+const updatePasses = async (latitude, longitude, altitude) => {
+
+  // Headers required for the proxy
+  let passesHeaders = new Headers();
+  passesHeaders.append("X-Requested-With", "fetch");
+  passesHeaders.append("Origin", "");
+
+  // Make request through a proxy
+  const passesRequest = new Request(
+    `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${
+      latitude // Interpolate data into request
+    }&lon=${
+      longitude // Interpolate data into request
+    }`,
+    { headers: passesHeaders },
+  );
+
+  while (true) {
+    try {
+      const passesResponse = await fetch(passesRequest);
+      break; // Leave while loop
+    } catch (e) {
+      if (!window.confirm("Encountered an issue during retrieval of passes information-- Retry?")) break; // Leave while loop
+      else throw e;
+    }
+  }
+
+  const passesObj = await passesResponse.json();
+
+  return passesObj.response;
+}
+
 class PassFinder extends Component {
   constructor(props) {
     super(props);
@@ -18,8 +94,8 @@ class PassFinder extends Component {
       longitude: undefined,
       altitude: undefined,
       nextPasses: [],
-      permissionGranted: false,
       positionRequested: false,
+      positionLoaded: false,
       passesLoaded: false,
     };
 
@@ -28,50 +104,14 @@ class PassFinder extends Component {
     );
   }
 
-  convertToRiseSet = (pass) => {
+  convertToRiseSet = () => {
     // TODO: Implement
   };
 
-  // TODO: Factor out everything except setting state
-  updatePosition = async () => {
+  update = async () => {
 
-    // Set getCurrentPosition options
-    const options = {
-      enableHighAccuracy: true,
-      timeout: 4000,
-      maximumAge: 0
-    };
-
-    // Access getCurrentPosition API
-    let position;
-    let tryAgain = true;
-
-    while (tryAgain) {
-      try {
-        position = await getCurrentPosition(options);
-      } catch (e) {
-
-        if (e.__proto__.constructor.name === "PositionError") {
-
-          if (e.code === 1) { // PERMISSION_DENIED
-            tryAgain = window.confirm(
-              "We need your permission to access your location information! Try again?"
-            );
-
-          } else if (e.code === 2) { // POSITION_UNAVAILABLE
-              tryAgain = window.confirm(
-                "The acquisition of the geolocation failed because at least one source of position on your machine machine failed! Try again?"
-              );
-
-          } else if (e.code === 3) { // TIMEOUT
-              tryAgain = window.confirm(
-                "We're having trouble finding you! Try again?"
-              );
-          }
-
-        } else throw e;
-      }
-    }
+    // Get user position
+    const position = await updatePosition();
 
     // Extract location data and assign it to component state
     this.setState({
@@ -81,49 +121,18 @@ class PassFinder extends Component {
       positionLoaded: true,
     });
 
-  }
-
-  // TODO: Factor out everything except setting state
-  updatePasses = async () => {
-    let passesHeaders = new Headers();
-    // Headers required for the proxy
-    passesHeaders.append("X-Requested-With", "fetch");
-    passesHeaders.append("Origin", "");
-
-    const passesRequest = new Request(
-      // Make request through a proxy
-      `https://cors-anywhere.herokuapp.com/http://api.open-notify.org/iss-pass.json?lat=${
-        this.state.latitude
-      }&lon=${
-        this.state.longitude
-      }`,
-      { headers: passesHeaders },
+    // Get the next passes over that location
+    const passes = await updatePasses(
+      this.state.latitude,
+      this.state.longitude,
+      this.state.altitude,
     );
 
-    let passesResponse;
-    try {
-      passesResponse = await fetch(passesRequest);
-    } catch (error) {
-      console.error(`ERROR: ${error.code} (${error.msg})`);
-      throw error;
-    }
+    this.setState({
+      nextPasses: passes,
+      passesLoaded: true,
+    });
 
-    let passesObj;
-    try {
-      passesObj = await passesResponse.json();
-      this.setState({
-        nextPasses: passesObj.response,
-        passesLoaded: true,
-      });
-    } catch (error) {
-      console.error(`ERROR: ${error.code} (${error.msg})`);
-      throw error;
-    }
-  }
-
-  update = async () => {
-    await this.updatePosition();
-    await this.updatePasses();
   }
 
   userRequestOwnGeoposition = () => {
@@ -141,11 +150,11 @@ class PassFinder extends Component {
 
     } else {
       return (
-        <PassesDisplay
+        <Presentational
           positionLoaded={this.state.positionLoaded}
+          passesLoaded={this.state.passesLoaded}
           latitude={this.state.latitude}
           longitude={this.state.longitude}
-          passesLoaded={this.state.passesLoaded}
           nextPass={this.state.nextPasses[0]}
         />
       );
@@ -155,47 +164,53 @@ class PassFinder extends Component {
 
 // TODO: Style this button
 function GeolocationRequestButton(props) {
-    return (
+  return (
+    <div>
+      <p>We'd like to get your location information so we can find the next pass the ISS will make over your position.
+      </p>
       <button
         onClick={props.requestPosition}
       >Get Location Information
       </button>
-    );
-
+    </div>
+  );
 }
 
-function PassesDisplay(props) {
+// TODO: round lat and long
+function Presentational(props) {
 
-  const positionDisplay = props.positionLoaded ?
-    (
+  const positionDisplay = ( props.positionLoaded
+    ? (
       <h1>Your location: latitude {props.latitude},
         longitude {props.longitude}
       </h1>
-    ) :
-    (<h1>Loading position data...</h1>);
+    ) : <h1>Loading position data...</h1>
+  );
 
+  // TODO: Implement conversion as function
   let nextPassDisplay;
+
   if (props.passesLoaded) {
-    // TODO: Implement conversion as function
+
+    // Convert risetime and duration to rise and set time
+    // and format the result
     let momentFmtString = "dddd, MMMM Do YYYY [at] h:mm a";
     let nextPassFmt = {};
     let riseTime = moment.unix(props.nextPass["risetime"]);
     nextPassFmt.riseTime = riseTime.format(momentFmtString);
-    nextPassFmt.setTime = riseTime.add(props.nextPass["duration"], 'seconds')
-      .format(momentFmtString);
+    nextPassFmt.setTime = riseTime.add(
+      props.nextPass["duration"],
+      'seconds',
+    ).format(momentFmtString);
 
+    // Create markup with this moment
     nextPassDisplay = (
-      <h1>The next time the ISS passes over your location,{' '}
-        it will rise on {nextPassFmt.riseTime}{' '}
-        and set on{' '}
-        {nextPassFmt.setTime}.
+      <h1>The next time the ISS passes over your location, it will rise on {nextPassFmt.riseTime} and set on {nextPassFmt.setTime}.
       </h1>
     );
-  } else {
-    nextPassDisplay = (
-        <h1>Loading passes data...</h1>
-      );
-  }
+
+    // Create markup to indicate loading state
+  } else nextPassDisplay = ( <h1>Loading passes data...</h1> );
 
   return (
     // TODO: Implement error boundaries
